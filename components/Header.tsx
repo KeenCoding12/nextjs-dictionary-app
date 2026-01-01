4035839a1843c98be3f4db9902d9b02455f64103
Keencoding <medhanyetsegay96@gmail.com>
2026-01-01 12:59:36 -0800

    Update Header.tsx
    Removed theme toggle functionality and related imports.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,7 @@
-"use client";
-import { Moon, Sun } from "lucide-react";
 
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 export default function Header() {
-  const { setTheme, resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
   return (
     <header>
-      <div className="container flex justify-between items-center py-6 md:py-8">
-        {/* Logo */}
-        <span className="text-2xl font-bold uppercase">Dicap</span>
-
-        {/* Theme toggle */}
-        <button
-          className={`size-11 flex bg-surface hover:bg-surface-muted items-center justify-center rounded-full   transition-colors`}
-          title="Theme Toggle"
-          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-        >
-          {resolvedTheme === "dark" ? <Moon /> : <Sun />}
-        </button>
-      </div>
     </header>
   );
 }
